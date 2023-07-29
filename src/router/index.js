5b7c191acecf7e8f7e151fa49a4674592716876e
Yjang Wynter <yjangwynter@gmail.com>
2023-07-29 17:50:01 -0400

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/website',
+      name: 'website',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/WebsiteView.vue')
+    }, 
+    {
+      path:'/linkedin',
+      name:'linkedin',
+      component: () => import('../views/LinkedinView.vue')
+    },
+    {
+      path:'/github',
+      name:'github',
+      component: () => import('../views/GithubView.vue')
+    },
+    { //Not Found
+      path: '/:pathMatch(.*)*', 
+      name:'NotFound', 
+      component: () => import('../views/WebsiteView.vue')
+    },
+  ]
+})
+
+export default router
---
+++ src/views/WebsiteView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="website">
+    <h1>Redirecting to https://yjangwynter.com</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script>
+window.location.href = "https://yjangwynter.com";
+</script>
